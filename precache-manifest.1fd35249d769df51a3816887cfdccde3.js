f31faee0adcda39c28127b7a1bad5da98ca54c39
aahmadali <aliyuahmad1996@gmail.com>
2022-05-12 17:11:13 +0100

    Updates

---
+++ precache-manifest.1fd35249d769df51a3816887cfdccde3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3301898083e9f32d6ee7",
+    "url": "/gitFriend/static/css/main.c3d6818a.chunk.css"
+  },
+  {
+    "revision": "3301898083e9f32d6ee7",
+    "url": "/gitFriend/static/js/main.33018980.chunk.js"
+  },
+  {
+    "revision": "cf6b6bf545d5975ec723",
+    "url": "/gitFriend/static/js/1.cf6b6bf5.chunk.js"
+  },
+  {
+    "revision": "0c705396068c71b29348",
+    "url": "/gitFriend/static/js/2.0c705396.chunk.js"
+  },
+  {
+    "revision": "91c59a1d5b092b5b0fe9",
+    "url": "/gitFriend/static/js/runtime~main.91c59a1d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gitFriend/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "04076df1fdd6f2afd7b78978e27f979f",
+    "url": "/gitFriend/index.html"
+  }
+];
